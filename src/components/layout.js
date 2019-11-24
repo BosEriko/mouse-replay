66ba1a112fb7234a1d28100511b99f040a13937b
BosEriko <bos.eriko@gmail.com>
2019-11-24 13:41:23 +0800

    add firebase entirely

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,18 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+import * as firebase from "firebase/app"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDEXdD9bnx-FA-aApEdv5Yx6Kz9n1gpE_k",
+  authDomain: "bos-eriko-reyes.firebaseapp.com",
+  databaseURL: "https://bos-eriko-reyes.firebaseio.com",
+  projectId: "bos-eriko-reyes",
+  storageBucket: "bos-eriko-reyes.appspot.com",
+  messagingSenderId: "170211165412",
+  appId: "1:170211165412:web:ff54a919a8d23622ab58f1"
+})
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
